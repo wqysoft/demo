6365d18023ad0e64a81d0e6ea605c7030aaabff4
wqysoft <wqysoft@163.com>
2019-05-28 11:23:27 +0800

    v-for newList

--- MIMS/src/components/News/Newslist.vue
+++ MIMS/src/components/News/Newslist.vue
@@ -1,117 +1,46 @@
 <template>
-  <div class="newList">
+  <div id="newList">
     <ul>
-      <li>
-        <a href="#">
-          <div class="new_img">
-            <img src="https://ss3.baidu.com/-fo3dSag_xI4khGko9WTAnF6hhy/image/h%3D300/sign=b5e4c905865494ee982209191df4e0e1/c2cec3fdfc03924590b2a9b58d94a4c27d1e2500.jpg" alt="">
-          </div>
-          <div class="content">
-            <h4 class="title">我爱你中国我爱你中国我爱你中国我爱你中国</h4>
-            <div class="news-desc">
-              <p class="summary">我的摘要内容我的摘要内容我的摘要内容我的摘要内容</p>
-              <p class="edit">
-                <span class="praise">点赞数:5</span>
-                <span class="time">发表时间:2017-08-09</span>
-              </p>
-            </div>
-          </div>
-        </a>
-      </li>
-       <li>
-        <a href="#">
-          <div class="new_img">
-            <img src="https://ss3.baidu.com/-fo3dSag_xI4khGko9WTAnF6hhy/image/h%3D300/sign=b5e4c905865494ee982209191df4e0e1/c2cec3fdfc03924590b2a9b58d94a4c27d1e2500.jpg" alt="">
-          </div>
-          <div class="content">
-            <h4 class="title">我爱你中国我爱你中国我爱你中国我爱你中国</h4>
-            <div class="news-desc">
-              <p class="summary">我的摘要内容我的摘要内容我的摘要内容我的摘要内容</p>
-              <p class="edit">
-                <span class="praise">点赞数:5</span>
-                <span class="time">发表时间:2017-08-09</span>
-              </p>
-            </div>
-          </div>
-        </a>
-      </li>
-       <li>
-        <a href="#">
-          <div class="new_img">
-            <img src="https://ss3.baidu.com/-fo3dSag_xI4khGko9WTAnF6hhy/image/h%3D300/sign=b5e4c905865494ee982209191df4e0e1/c2cec3fdfc03924590b2a9b58d94a4c27d1e2500.jpg" alt="">
-          </div>
-          <div class="content">
-            <h4 class="title">我爱你中国我爱你中国我爱你中国我爱你中国</h4>
-            <div class="news-desc">
-              <p class="summary">我的摘要内容我的摘要内容我的摘要内容我的摘要内容</p>
-              <p class="edit">
-                <span class="praise">点赞数:5</span>
-                <span class="time">发表时间:2017-08-09</span>
-              </p>
-            </div>
-          </div>
-        </a>
-      </li>
-       <li>
-        <a href="#">
-          <div class="new_img">
-            <img src="https://ss3.baidu.com/-fo3dSag_xI4khGko9WTAnF6hhy/image/h%3D300/sign=b5e4c905865494ee982209191df4e0e1/c2cec3fdfc03924590b2a9b58d94a4c27d1e2500.jpg" alt="">
-          </div>
-          <div class="content">
-            <h4 class="title">我爱你中国我爱你中国我爱你中国我爱你中国</h4>
-            <div class="news-desc">
-              <p class="summary">我的摘要内容我的摘要内容我的摘要内容我的摘要内容</p>
-              <p class="edit">
-                <span class="praise">点赞数:5</span>
-                <span class="time">发表时间:2017-08-09</span>
-              </p>
-            </div>
-          </div>
-        </a>
-      </li>
-       <li>
-        <a href="#">
-          <div class="new_img">
-            <img src="https://ss3.baidu.com/-fo3dSag_xI4khGko9WTAnF6hhy/image/h%3D300/sign=b5e4c905865494ee982209191df4e0e1/c2cec3fdfc03924590b2a9b58d94a4c27d1e2500.jpg" alt="">
-          </div>
-          <div class="content">
-            <h4 class="title">我爱你中国我爱你中国我爱你中国我爱你中国</h4>
-            <div class="news-desc">
-              <p class="summary">我的摘要内容我的摘要内容我的摘要内容我的摘要内容</p>
-              <p class="edit">
-                <span class="praise">点赞数:5</span>
-                <span class="time">发表时间:2017-08-09</span>
-              </p>
-            </div>
-          </div>
-        </a>
-      </li>
-       <li>
-        <a href="#">
-          <div class="new_img">
-            <img src="https://ss3.baidu.com/-fo3dSag_xI4khGko9WTAnF6hhy/image/h%3D300/sign=b5e4c905865494ee982209191df4e0e1/c2cec3fdfc03924590b2a9b58d94a4c27d1e2500.jpg" alt="">
-          </div>
-          <div class="content">
-            <h4 class="title">我爱你中国我爱你中国我爱你中国我爱你中国</h4>
-            <div class="news-desc">
-              <p class="summary">我的摘要内容我的摘要内容我的摘要内容我的摘要内容</p>
-              <p class="edit">
-                <span class="praise">点赞数:5</span>
-                <span class="time">发表时间:2017-08-09</span>
-              </p>
-            </div>
-          </div>
-        </a>
-      </li>
+     <NewList></NewList>
+     <NewList></NewList>
+     <NewList></NewList>
+     <NewList></NewList>
+     <NewList></NewList>
+     <NewList></NewList>
+     <NewList></NewList>
+     <NewList></NewList>
     </ul>
   </div>
 </template>
 <script>
+var NewList={
+  template:`
+   <li style="margin-top: 20px;">
+    <a href="#">
+      <div class="new_img" style="float: left;margin-right:20px;">
+        <img style="width: 100px;height: 100px;margin-top: 10px;" src="https://ss3.baidu.com/-fo3dSag_xI4khGko9WTAnF6hhy/image/h%3D300/sign=b5e4c905865494ee982209191df4e0e1/c2cec3fdfc03924590b2a9b58d94a4c27d1e2500.jpg" alt="">
+      </div>
+      <div class="content">
+        <h4 class="title">我爱你中国我爱你中国我爱你中国我爱你中国</h4>
+        <div class="news-desc">
+          <p class="summary" style="color:#000;">我的摘要内容我的摘要内容我的摘要内容我的摘要内容</p>
+          <p class="edit" style="color:coral;">
+            <span class="praise" style="margin-right: 10px;">点赞数:5</span>
+            <span class="time">发表时间:2017-08-09</span>
+          </p>
+        </div>
+      </div>
+    </a>
+  </li>
+  `
+}
 export default {
   data() {
     return {
- newsList:[]
     };
+  },
+  components:{
+    NewList
   },
 created(){
    this.$axios.get('')
@@ -128,28 +57,5 @@ created(){
 };
 </script>
 <style scoped>
-ul li img{
-   width: 100px;
-   height: 100px;
-   margin-top: 10px;
-}
-ul li{
-margin-top: 20px;
-}
-ul li .new_img{
-   float: left;
-   margin-right:20px;
-}
-ul li .content .summary{
-   margin-top: 10px;
-}
-ul li .content .praise{
-   margin-right: 10px;
-}
-ul li .content{
-   color:#000;
-}
-ul li .content .edit{
-   color:coral;
-}
+
 </style>
