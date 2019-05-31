8e34cf16599a874583ec056651d5f8d5c050c125
wqysoft <wqysoft@163.com>
2019-05-31 16:37:37 +0800

    fix bug

--- MIMS/src/components/News/Newslist.vue
+++ MIMS/src/components/News/Newslist.vue
@@ -15,14 +15,14 @@
 <script>
 var NewList={
   template:`
-   <li style="margin-top: 20px;">
+   <li>
     <a href="#">
       <div class="new_img" style="float: left;margin-right:20px;">
-        <img style="width: 100px;height: 100px;margin-top: 10px;" src="https://ss3.baidu.com/-fo3dSag_xI4khGko9WTAnF6hhy/image/h%3D300/sign=b5e4c905865494ee982209191df4e0e1/c2cec3fdfc03924590b2a9b58d94a4c27d1e2500.jpg" alt="">
+        <img style="width: 100px;height: 100px;margin-top: 3px;" src="https://ss3.baidu.com/-fo3dSag_xI4khGko9WTAnF6hhy/image/h%3D300/sign=b5e4c905865494ee982209191df4e0e1/c2cec3fdfc03924590b2a9b58d94a4c27d1e2500.jpg" alt="">
       </div>
       <div class="content">
-        <h4 class="title">我爱你中国我爱你中国我爱你中国我爱你中国</h4>
-        <div class="news-desc">
+        <h4>我爱你中国我爱你中国我爱你中国我爱你中国</h4>
+        <div>
           <p class="summary" style="color:#000;">我的摘要内容我的摘要内容我的摘要内容我的摘要内容</p>
           <p class="edit" style="color:coral;">
             <span class="praise" style="margin-right: 10px;">点赞数:5</span>
@@ -57,5 +57,8 @@ created(){
 };
 </script>
 <style scoped>
+ul li{
+margin-top: 20px;
+}
 
 </style>
